a552377cdcfced4eee705f42578ee3850651dfce
NyawiraMuturi <albinanyawira@gmail.com>
2022-05-30 22:28:57 +0300

    learn lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import HemisphereDisplay from "./Components/HemisphereDisplay";
+import React, { Component } from 'react'
 
+export class App extends Component {
 
-function App() {
+  state ={
+    latitude: null,
+    errorMessage: ''
+  }
 
-  return (
-    <>
-    <HemisphereDisplay/>
-    </>
-  );
+  componentDidMount(){
+    console.log("component did mount")
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({latitude: position.coords.latitude})
+      },
+      (error)=> {
+        this.setState({errorMessage: error.message})
+      }
+    );
+  }
+
+  componentDidUpdate(){
+    console.log("component did update")
+  }
+
+  render() {
+
+    
+
+    if (this.state.latitude && !this.state.errorMessage){
+      return <div> <HemisphereDisplay latitude={this.state.latitude}/> </div>
+    }
+    if (!this.state.latitude && this.state.errorMessage){
+      return <div>{this.state.errorMessage}</div>
+    }
+    else{
+      return <div>Loading...</div>
+    }
+  }
 }
 
 export default App;
--- src/Components/HemisphereDisplay.js
+++ src/Components/HemisphereDisplay.js
@@ -1,40 +1,18 @@
-import { Divider } from '@material-ui/core';
-import React, { Component } from 'react'
+import React from 'react'
 
-export class HemisphereDisplay extends Component {
+const HemisphereDisplay = ({latitude}) => {
+  console.log(latitude)
+  const hemisphere =latitude > 0 ? 'Nothern Hemisphere' : 'Southern Hemisphere'
 
-  constructor(props){
-    super(props)
-
-    this.state ={
-      latitude: null,
-      errorMessage: ''
-    }
-
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({latitude: position.coords.latitude})
-      },
-      (error)=> {
-        this.setState({errorMessage: error.message})
-      }
-    );
-  }
-
-  render() {
-    if (this.state.latitude && !this.state.errorMessage){
-      return <div>{this.state.latitude}</div>
-    }
-    if (!this.state.latitude && this.state.errorMessage){
-      return <div>{this.state.errorMessage}</div>
-    }
-    else{
-      return <div>Loading...</div>
-    }
-  }
+  return (
+    <div>
+      I am in the northern Hemisphere
+    </div>
+  )
 }
 
 export default HemisphereDisplay
+
 
 
 
