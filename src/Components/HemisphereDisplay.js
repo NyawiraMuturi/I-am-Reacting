25d7e707bc8189a98e51efccc9eb6a34d22404d1
NyawiraMuturi <albinanyawira@gmail.com>
2022-06-06 11:32:04 +0300

    add hemisphere logic

--- src/Components/HemisphereDisplay.js
+++ src/Components/HemisphereDisplay.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import north from '/home/nyawira/Documents/Personal projects/ReactPractice/practice/src/img/north.jpeg'
+import south from '/home/nyawira/Documents/Personal projects/ReactPractice/practice/src/img/south.jpeg'
+import './HemisphereDisplay.css'
+
+const hemisphereConfig = {
+  Nothern: {
+    text: "You Are in the Nothern Hemisphere",
+    pic: north
+  },
+  Southern: {
+    text: "You Are in the Southern Hemisphere",
+    pic: south
+  }
+}
 
 const HemisphereDisplay = ({latitude}) => {
   console.log(latitude)
-  const hemisphere =latitude > 0 ? 'Nothern Hemisphere' : 'Southern Hemisphere'
+  const hemisphere =latitude > 0 ? 'Nothern' : 'Southern';
+  const {text, pic} = hemisphereConfig[hemisphere]
 
   return (
     <div>
-      I am in the northern Hemisphere
+      <img src={pic} alt="" className='picture'/>
+      {text}
     </div>
   )
 }
